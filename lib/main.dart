cd0a497f5f1c9c8864d07ddb84e064577fc91889
rooshimadethis <rooshipatidar@gmail.com>
2025-12-29 22:55:43 -0600

    api key

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:ash_trainer/core/theme/app_theme.dart';
 
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const ProviderScope(child: AshTrainerApp()));
 }
 
